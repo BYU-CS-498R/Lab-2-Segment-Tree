8ae12bb7995fd3ad4770edb3b67f33480b20627e
cbhomestar <cody.burt.21@gmail.com>
2016-11-08 14:28:27 -0700

    starter code

---
+++ main.py
@@ -0,0 +1,15 @@
+# You shouldn't need to modify this file at all.
+
+import unittest
+# Imports test cases
+from code.segmenttree.tests.module_tests import *
+
+
+def main():
+    """Main function for running test code.
+    """
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
